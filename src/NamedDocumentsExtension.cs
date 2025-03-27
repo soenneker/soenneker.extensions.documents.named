56e4fe85098ed6d4a9b44f8205e6a2f31a1e64fd
Jake Soenneker <jake@soenneker.com>
2025-03-27 09:14:42 -0500

    Initial commit

---
+++ src/NamedDocumentsExtension.cs
@@ -0,0 +1,15 @@
+﻿using Soenneker.Documents.Named.Abstract;
+using Soenneker.Dtos.IdNamePair;
+
+namespace Soenneker.Extensions.Documents.Named;
+
+/// <summary>
+/// A collection of helpful NamedDocument extension methods
+/// </summary>
+public static class NamedDocumentsExtension
+{
+    public static IdNamePair ToIdNamePair<T>(this T value) where T : INamedDocument
+    {
+        return new IdNamePair { Id = value.Id, Name = value.Name };
+    }
+}
---
+++ test/Soenneker.Extensions.Documents.Named.Tests/NamedDocumentsExtensionTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Documents.Named.Tests;
+
+public sealed class NamedDocumentsExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
