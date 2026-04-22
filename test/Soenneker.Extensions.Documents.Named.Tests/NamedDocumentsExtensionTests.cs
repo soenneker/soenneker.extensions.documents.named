4a4d515b7253089cef8875e828bc36330602a11e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:54 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Documents.Named.Tests/NamedDocumentsExtensionTests.cs
+++ test/Soenneker.Extensions.Documents.Named.Tests/NamedDocumentsExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.Documents.Named.Tests;
 
 public sealed class NamedDocumentsExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
